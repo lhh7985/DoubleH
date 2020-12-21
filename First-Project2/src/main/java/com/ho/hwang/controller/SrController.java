b1a2d2a73abbf238ceb151e2e3126574dc9c11a2
DoubleH <ghghkd52@naver.com>
2020-12-21 13:12:04 +0900

    refactor : Service Request 클래스 - mapper 분리, jqGrid 적용, VO에 빌더 적용

--- First-Project2/src/main/java/com/ho/hwang/controller/SrController.java
+++ First-Project2/src/main/java/com/ho/hwang/controller/SrController.java
@@ -1,31 +1,23 @@
 package com.ho.hwang.controller;
 
-import java.security.Principal;
-import java.util.List;
-import java.util.Optional;
-
-import com.ho.hwang.dto.Activity.SelectCustomerActivityDto;
 import com.ho.hwang.dto.Sr.InsertSrDto;
 import com.ho.hwang.dto.Sr.SelectSrDetailDto;
 import com.ho.hwang.dto.Sr.SelectSrDto;
-import com.ho.hwang.paging.Page;
-import com.ho.hwang.service.ProductService;
-import com.ho.hwang.vo.ActivityVo;
-import com.ho.hwang.vo.SrVo;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import com.ho.hwang.dto.Sr.UpdateSrDetailDto;
+import com.ho.hwang.paging.JqgridResponse;
 import com.ho.hwang.service.ActivityService;
+import com.ho.hwang.service.ProductService;
 import com.ho.hwang.service.SrService;
 import com.ho.hwang.service.UserService;
-
+import com.ho.hwang.vo.ActivityVo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Controller
 @RequiredArgsConstructor
@@ -38,16 +30,17 @@ public class SrController {
 	private final ProductService productService;
 
 	@GetMapping("/list")
-	public String getSrList(@RequestParam(defaultValue = "1") int page, Model model) {
-
-		int listCnt = srService.selectSrTotalCount();
-		Page paging = new Page(listCnt, page);
-
-		List<SelectSrDto> srList = srService.selectSR(paging.getStartIndex(), paging.getPageSize());
-		model.addAttribute("srList", srList);
-		model.addAttribute("paging", paging);
-
+	public String getSrList(Principal principal) {
 		return "sr/list";
+	}
+
+	@GetMapping("/getlist")
+	public @ResponseBody
+	JqgridResponse getAll() {
+		List<SelectSrDto> srList = srService.selectSR();
+		JqgridResponse response = new JqgridResponse();
+		response.setRows(srList);
+		return response;
 	}
 
 	// ============================등록 활동 sr
@@ -68,11 +61,31 @@ public class SrController {
 		SelectSrDetailDto selectSrDetailDto = srService.selectSRDetail(srId)
 				.map((srVo)->new SelectSrDetailDto(srVo))
 				.orElseThrow(() -> new NoSuchElementException());
-				model.addAttribute("srvo", selectSrDetailDto);
+		model.addAttribute("srvo", selectSrDetailDto);
+
 
 		List<ActivityVo> activityVo = activityService.selectCustomerActivity(srId);
-			model.addAttribute("acvo", activityVo);
+		model.addAttribute("acvo", activityVo);
 
-			return "sr/detail";
+		return "sr/detail";
+	}
+
+	@GetMapping("/delete/{srId}")
+	@ResponseBody
+	public int deleteSr(@PathVariable("srId") int srId){
+		int result = srService.deleteSr(srId);
+		return result;
+	}
+
+	@PostMapping("/complete/{srId}")
+	@ResponseBody
+	public int setSrCompletion(@PathVariable("srId") int srId, Principal principal){
+		return srService.updateSrComplete(srId, principal);
+	}
+
+	@PostMapping("/update")
+	@ResponseBody
+	public int modifySrDetail(UpdateSrDetailDto updateSrDetailDto, Principal principal){
+		return srService.updateSrDetail(updateSrDetailDto, principal);
 	}
 }
